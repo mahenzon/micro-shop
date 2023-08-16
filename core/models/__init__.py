593676d0e8b92998ba2831bdb7dc61ae429e7d67
Suren Khorenyan <surenkhorenyan@gmail.com>
2023-08-16 21:55:10 +0300

    create base model

---
+++ core/models/__init__.py
@@ -0,0 +1,10 @@
+__all__ = (
+    "Base",
+    "DatabaseHelper",
+    "db_helper",
+    "Product",
+)
+
+from .base import Base
+from .db_helper import DatabaseHelper, db_helper
+from .product import Product
---
+++ core/models/base.py
@@ -0,0 +1,11 @@
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, declared_attr
+
+
+class Base(DeclarativeBase):
+    __abstract__ = True
+
+    @declared_attr.directive
+    def __tablename__(cls) -> str:
+        return f"{cls.__name__.lower()}s"
+
+    id: Mapped[int] = mapped_column(primary_key=True)

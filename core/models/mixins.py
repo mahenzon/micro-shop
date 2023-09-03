c62582ebb213404eb2b6e008aee4705d93b655bb
Suren Khorenyan <surenkhorenyan@gmail.com>
2023-09-03 19:07:32 +0300

    create user relation mixin

---
+++ core/models/mixins.py
@@ -0,0 +1,28 @@
+from typing import TYPE_CHECKING
+
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import declared_attr, Mapped, mapped_column, relationship
+
+if TYPE_CHECKING:
+    from .user import User
+
+
+class UserRelationMixin:
+    _user_id_nullable: bool = False
+    _user_id_unique: bool = False
+    _user_back_populates: str | None = None
+
+    @declared_attr
+    def user_id(cls) -> Mapped[int]:
+        return mapped_column(
+            ForeignKey("users.id"),
+            unique=cls._user_id_unique,
+            nullable=cls._user_id_nullable,
+        )
+
+    @declared_attr
+    def user(cls) -> Mapped["User"]:
+        return relationship(
+            "User",
+            back_populates=cls._user_back_populates,
+        )
--- core/models/post.py
+++ core/models/post.py
@@ -1,23 +1,18 @@
-from typing import TYPE_CHECKING
-
-from sqlalchemy import String, Text, ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import String, Text
+from sqlalchemy.orm import Mapped, mapped_column
 
 from .base import Base
-
-if TYPE_CHECKING:
-    from .user import User
+from .mixins import UserRelationMixin
 
 
-class Post(Base):
+class Post(UserRelationMixin, Base):
+    # _user_id_nullable = False
+    # _user_id_unique = False
+    _user_back_populates = "posts"
+
     title: Mapped[str] = mapped_column(String(100), unique=False)
     body: Mapped[str] = mapped_column(
         Text,
         default="",
         server_default="",
     )
-    user_id: Mapped[int] = mapped_column(
-        ForeignKey("users.id"),
-        # nullable=False,
-    )
-    user: Mapped["User"] = relationship(back_populates="posts")
